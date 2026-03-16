91f55958aeffdaf5880418fe1968fa4612b2366a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-03-15 19:13:25 -0700

    style(apps): run pre-commit

--- backend/src/config/settings/base.py
+++ backend/src/config/settings/base.py
@@ -18,6 +18,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local apps
+    "apps.assessments.apps.AssessmentsConfig",
+    "apps.grades.apps.GradesConfig",
+    "apps.students.apps.StudentsConfig",
+    "apps.terms.apps.TermsConfig",
 ]
 
 MIDDLEWARE = [
