a07e8d8b4ee30d8f38106ddeba56254a3543dfc1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-03-15 18:47:19 -0700

    fix(base): fix database and static

--- backend/src/config/settings/base.py
+++ backend/src/config/settings/base.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", ""),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
     }
 }
 
@@ -79,5 +83,4 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = STATIC_ROOT = os.path.join(STATIC_DIR, "")
-STATIC_URL = os.path.join(STATIC_DIR, "")
+STATIC_URL = "/static/"
