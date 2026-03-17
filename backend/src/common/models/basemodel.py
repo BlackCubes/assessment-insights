658169094df1956f5ac0211d57df12f91fc75cdd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-03-16 18:51:22 -0700

    style(base-model): run pre-commit

--- backend/src/common/models/basemodel.py
+++ backend/src/common/models/basemodel.py
@@ -1,11 +1,13 @@
-from django.db import models
 import uuid
+
+from django.db import models
+
 
 # TODO: Add `active`, `owner`, and `is_deleted` fields
 class BaseModel(models.Model):
-  uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-  created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-  updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
-  class Meta:
-    abstract = True
+    class Meta:
+        abstract = True
