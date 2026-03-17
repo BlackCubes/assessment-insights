4c96b8721ebb18360be4af7b958b58e9161e2b30
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-03-16 19:34:54 -0700

    feat(apps): create new academics app

--- backend/src/config/settings/base.py
+++ backend/src/config/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local apps
+    "apps.academics.apps.AcademicsConfig",
     "apps.assessments.apps.AssessmentsConfig",
     "apps.grades.apps.GradesConfig",
     "apps.students.apps.StudentsConfig",
