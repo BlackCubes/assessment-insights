b173826376ad8aaf7f66fc30ed5fe8618718cfbb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-03-16 18:52:36 -0700

    feat(student-models): add student model

--- backend/src/apps/students/models/student.py
+++ backend/src/apps/students/models/student.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class Student(BaseModel):
+    class GenderTypes(models.TextChoices):
+        Male = "M", _("Male")
+        Female = "F", _("Female")
+        Other = "O", _("Other")
+
+    student_id = models.CharField(unique=True, max_length=10)
+    full_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=2, choices=GenderTypes)
