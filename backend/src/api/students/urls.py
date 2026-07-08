08ce3ec863080bb648f919f6256f0ee7640061eb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-08 11:07:27 -0700

    style(urls-students-api): run pre-commit

--- backend/src/api/students/urls.py
+++ backend/src/api/students/urls.py
@@ -4,6 +4,12 @@ from .views import StudentDetailUpdateView, StudentListCreateView
 
 urlpatterns = [
     path("", StudentListCreateView.as_view(), name="student-list-create"),
-    path("<uuid:uuid>/", StudentDetailUpdateView.as_view(), name="student-detail-update"),
-    path("<int:student_id>/", StudentDetailUpdateView.as_view(), name="student-detail-update"),
+    path(
+        "<uuid:uuid>/", StudentDetailUpdateView.as_view(), name="student-detail-update"
+    ),
+    path(
+        "<int:student_id>/",
+        StudentDetailUpdateView.as_view(),
+        name="student-detail-update",
+    ),
 ]
