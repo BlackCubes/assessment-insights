6065481fa3a3e5c419443eea486ce151822fec54
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-08 09:01:14 -0700

    feat(student-views-students): add student-detail-view

--- backend/src/api/students/views/student.py
+++ backend/src/api/students/views/student.py
@@ -2,9 +2,16 @@ from rest_framework import generics, permissions
 
 from api.students.serializers import StudentSerializer
 from apps.students.models import Student
+from common.mixins import MultipleFieldLookupMixin
 
 
 class StudentListView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
     queryset = Student.objects.all().order_by("full_name")
     serializer_class = StudentSerializer
+
+
+class StudentDetailView(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    permission_classes = [permissions.AllowAny]
+    queryset = Student.objects.all()
+    lookup_fields = ["uuid", "student_id"]
