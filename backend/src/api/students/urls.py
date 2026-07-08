3cdb908ac2a69d38db53a335e7eedee848ddbcd7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-08 09:54:45 -0700

    feat(urls-students-api): add route for student detail

--- backend/src/api/students/urls.py
+++ backend/src/api/students/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import StudentListView
+from .views import StudentDetailView, StudentListView
 
 urlpatterns = [
     path("", StudentListView.as_view(), name="student-list"),
+    path("<uuid:uuid>/", StudentDetailView.as_view(), name="student-detail"),
+    path("<int:student_id>/", StudentDetailView.as_view(), name="student-detail"),
 ]
