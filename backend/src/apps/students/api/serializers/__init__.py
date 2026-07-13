ec60249e0d8de9b88b863c5438d8ce9960fed520
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-13 10:43:04 -0700

    refactor(student-create-serializer): remove unused file

--- backend/src/apps/students/api/serializers/__init__.py
+++ backend/src/apps/students/api/serializers/__init__.py
@@ -1,4 +1,3 @@
 from .student import StudentSerializer
-from .student_create import StudentCreateSerializer
 
-__all__ = ["StudentSerializer", "StudentCreateSerializer"]
+__all__ = ["StudentSerializer"]
