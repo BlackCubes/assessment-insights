3b9a2390762ca287f6b513a34dcd0701be95da61
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-13 10:50:05 -0700

    style(student-collection-views): run pre-commit

--- backend/src/apps/students/api/views/student_collection.py
+++ backend/src/apps/students/api/views/student_collection.py
@@ -1,10 +1,12 @@
+from django.db.models import QuerySet
 from rest_framework import generics, permissions
+
 from apps.students.api.serializers import StudentSerializer
 from apps.students.models import Student
 from apps.students.selectors import get_students
-from django.db.models import QuerySet
+
 
 class StudentListCreateView(generics.ListCreateAPIView[Student]):
-  permission_classes = [permissions.AllowAny]
-  serializer_class = StudentSerializer
-  queryset: QuerySet[Student] = get_students()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StudentSerializer
+    queryset: QuerySet[Student] = get_students()
