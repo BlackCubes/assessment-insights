b329925a7005ce07ca6724d33378ddc041c84271
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-14 11:16:28 -0700

    feat(update-period-services): add services

--- backend/src/apps/academics/services/__init__.py
+++ backend/src/apps/academics/services/__init__.py
@@ -1,3 +1,4 @@
 from .create_period import CreatePeriodData, create_period
+from .update_period import UpdatePeriodData, update_period
 
-__all__ = ["CreatePeriodData", "create_period"]
+__all__ = ["CreatePeriodData", "UpdatePeriodData", "create_period", "update_period"]
