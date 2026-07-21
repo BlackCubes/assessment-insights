5394214b53988d2be34c66c7a7607fb6f4fb56de
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 15:29:24 -0700

    feat(update-term-services): add update service for term

--- backend/src/apps/academics/services/__init__.py
+++ backend/src/apps/academics/services/__init__.py
@@ -1,12 +1,15 @@
 from .create_period import CreatePeriodData, create_period
 from .create_term import CreateTermData, create_term
 from .update_period import UpdatePeriodData, update_period
+from .update_term import UpdateTermData, update_term
 
 __all__ = [
     "CreatePeriodData",
     "CreateTermData",
     "UpdatePeriodData",
+    "UpdateTermData",
     "create_period",
     "create_term",
     "update_period",
+    "update_term",
 ]
