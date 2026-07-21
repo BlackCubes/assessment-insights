db2da8cb5fc96a0c101132b34dc1281ba7787ce3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 16:07:29 -0700

    feat(term-collection-views): add view for list-create

--- backend/src/apps/academics/api/views/__init__.py
+++ backend/src/apps/academics/api/views/__init__.py
@@ -1,4 +1,5 @@
 from .period_collection import PeriodListCreateView
 from .period_detail import PeriodDetailView
+from .term_collection import TermListCreateView
 
-__all__ = ["PeriodDetailView", "PeriodListCreateView"]
+__all__ = ["PeriodDetailView", "PeriodListCreateView", "TermListCreateView"]
