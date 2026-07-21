286e9a5c1ba77f6c9a2ef07bf77e981bca5818a8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 14:47:24 -0700

    fix(api-urls): use correct variable name

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 app_name = "api"
 
-url_patterns = [
+urlpatterns = [
     path("academics/", include("apps.academics.api.urls", namespace="academics")),
     path("students/", include("apps.students.api.urls", namespace="students")),
 ]
