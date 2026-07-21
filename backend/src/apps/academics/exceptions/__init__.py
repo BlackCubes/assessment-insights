2ca1f3b9195f0df14c5192ec58ce2ce4842beb50
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 15:12:19 -0700

    feat(terms-exceptions-academics): add exceptions

--- backend/src/apps/academics/exceptions/__init__.py
+++ backend/src/apps/academics/exceptions/__init__.py
@@ -1,4 +1,11 @@
 from .base import AcademicsException
 from .periods import PeriodAlreadyExistsException, PeriodException
+from .terms import TermAlreadyExistsException, TermException
 
-__all__ = ["AcademicsException", "PeriodException", "PeriodAlreadyExistsException"]
+__all__ = [
+    "AcademicsException",
+    "PeriodException",
+    "PeriodAlreadyExistsException",
+    "TermException",
+    "TermAlreadyExistsException",
+]
