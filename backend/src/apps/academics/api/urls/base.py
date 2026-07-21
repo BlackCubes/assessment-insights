244d5c7ca71f7a1842c84d855898697849640065
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 16:17:31 -0700

    feat(terms-urls-api): add URL under academics app

--- backend/src/apps/academics/api/urls/base.py
+++ backend/src/apps/academics/api/urls/base.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 app_name = "academics"
 
 urlpatterns = [
-    path("periods/", include("apps.academics.api.urls.periods", namespace="periods"))
+    path("periods/", include("apps.academics.api.urls.periods", namespace="periods")),
+    path("terms/", include("apps.academics.api.urls.terms", namespace="terms")),
 ]
