7283aaf802bd4ef5c04aa6a917b285df2b21eb14
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-07-21 15:19:04 -0700

    feat(create-term-services): add creation service for term

--- backend/src/apps/academics/services/__init__.py
+++ backend/src/apps/academics/services/__init__.py
@@ -1,4 +1,12 @@
 from .create_period import CreatePeriodData, create_period
+from .create_term import CreateTermData, create_term
 from .update_period import UpdatePeriodData, update_period
 
-__all__ = ["CreatePeriodData", "UpdatePeriodData", "create_period", "update_period"]
+__all__ = [
+    "CreatePeriodData",
+    "CreateTermData",
+    "UpdatePeriodData",
+    "create_period",
+    "create_term",
+    "update_period",
+]
