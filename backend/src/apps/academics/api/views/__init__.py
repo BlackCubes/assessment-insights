b9cba88f0d8460b57834faf5a8834320b3c2ac0a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-08-10 15:51:36 -0700

    feat(student-enrollment-views): add collection and detail views

--- backend/src/apps/academics/api/views/__init__.py
+++ backend/src/apps/academics/api/views/__init__.py
@@ -1,11 +1,15 @@
 from .period_collection import PeriodListCreateView
 from .period_detail import PeriodDetailView
+from .student_period_enrollment_collection import StudentPeriodEnrollmentListCreateView
+from .student_period_enrollment_detail import StudentPeriodEnrollmentDetailView
 from .term_collection import TermListCreateView
 from .term_detail import TermDetailView
 
 __all__ = [
     "PeriodDetailView",
     "PeriodListCreateView",
+    "StudentPeriodEnrollmentDetailView",
+    "StudentPeriodEnrollmentListCreateView",
     "TermDetailView",
     "TermListCreateView",
 ]
---
+++ backend/src/apps/academics/api/views/student_period_enrollment_detail.py
@@ -0,0 +1,13 @@
+from django.db.models import QuerySet
+from rest_framework import generics, permissions
+
+from apps.academics.api.serializers import StudentPeriodEnrollmentSerializer
+from apps.academics.models import StudentPeriodEnrollment
+from apps.academics.selectors import get_student_period_enrollments
+
+
+class StudentPeriodEnrollmentDetailView(generics.RetrieveUpdateAPIView[StudentPeriodEnrollment]):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StudentPeriodEnrollmentSerializer
+    queryset: QuerySet[StudentPeriodEnrollment] = get_student_period_enrollments()
+    lookup_field = "uuid"
