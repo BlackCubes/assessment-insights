85becd93d6627fa2b57f8d08d2b5e96398165732
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-08-10 16:05:08 -0700

    feat(student-enrollment-urls): add url

--- backend/src/apps/academics/api/urls/base.py
+++ backend/src/apps/academics/api/urls/base.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 app_name = "academics"
 
 urlpatterns = [
+    path("enrollments/", include("apps.academics.api.urls.student_period_enrollments", namespace="enrollments")),
     path("periods/", include("apps.academics.api.urls.periods", namespace="periods")),
     path("terms/", include("apps.academics.api.urls.terms", namespace="terms")),
 ]
---
+++ backend/src/apps/academics/api/urls/student_period_enrollments.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from apps.academics.api.views import StudentPeriodEnrollmentDetailView, StudentPeriodEnrollmentListCreateView
+
+app_name = "enrollments"
+
+urlpatterns = [
+  path("", StudentPeriodEnrollmentListCreateView.as_view(), name="list-create"),
+  path("<uuid:uuid>/", StudentPeriodEnrollmentDetailView.as_view(), name="detail")
+]
