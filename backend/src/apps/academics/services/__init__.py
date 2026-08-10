1224c93fe6d53e2ec9ad3faea78083ad0edac2b5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-08-10 14:27:38 -0700

    feat(create-student-enrollment): add creation functionality

--- backend/src/apps/academics/services/__init__.py
+++ backend/src/apps/academics/services/__init__.py
@@ -1,14 +1,20 @@
 from .create_period import CreatePeriodData, create_period
+from .create_student_period_enrollment import (
+    CreateStudentPeriodEnrollmentData,
+    create_student_period_enrollment,
+)
 from .create_term import CreateTermData, create_term
 from .update_period import UpdatePeriodData, update_period
 from .update_term import UpdateTermData, update_term
 
 __all__ = [
     "CreatePeriodData",
+    "CreateStudentPeriodEnrollmentData",
     "CreateTermData",
     "UpdatePeriodData",
     "UpdateTermData",
     "create_period",
+    "create_student_period_enrollment",
     "create_term",
     "update_period",
     "update_term",
