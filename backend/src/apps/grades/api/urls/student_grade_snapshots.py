b1729e7c26c8d12cd884fbfcff2a8d0dc80d8595
Elias Gutierrez <gutierrezelias1991@gmail.com>
2026-08-11 13:59:18 -0700

    feat(urls-api-grades): add bulk creation feature

--- backend/src/apps/grades/api/urls/student_grade_snapshots.py
+++ backend/src/apps/grades/api/urls/student_grade_snapshots.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from apps.grades.api.views import (
+    StudentGradeSnapshotBulkCreateView,
     StudentGradeSnapshotDetailView,
     StudentGradeSnapshotListCreateView,
 )
 
 urlpatterns = [
     path("", StudentGradeSnapshotListCreateView.as_view(), name="list-create"),
+    path("bulk/", StudentGradeSnapshotBulkCreateView.as_view(), name="bulk-create"),
     path("<uuid:uuid>/", StudentGradeSnapshotDetailView.as_view(), name="detail"),
 ]
